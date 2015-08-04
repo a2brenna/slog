c999cbb80c231720e4516a1f1acd2a759b1c1bde
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-08-03 20:47:20 -0400

    Re-define how logging should work

--- src/slog.cc
+++ src/slog.cc
@@ -1,74 +1,7 @@
 #include "slog.h"
 
-bool syslog_initialized = false;
-
 namespace slog{
 
-Syslog::Syslog(const Priority& priority, const std::string &ident, const int &facility) {
-    if(!syslog_initialized){
-        openlog(ident.c_str(), LOG_PID, facility);
-        syslog_initialized = true;
-    }
-    _priority = priority;
-    _header = "";
-}
-
-Syslog::Syslog(const Priority& priority, const std::string &header, const std::string &ident, const int &facility) {
-    if(!syslog_initialized){
-        openlog(ident.c_str(), LOG_PID, facility);
-        syslog_initialized = true;
-    }
-    _priority = priority;
-    _header = header;
-}
-
-int Syslog::sync() {
-    if (!_buffer.empty() && (_priority <= GLOBAL_PRIORITY)) {
-        syslog(_priority, "%s%s", _header.c_str(), _buffer.c_str());
-    }
-    _buffer.erase();
-    return 0;
-}
-
-int Syslog::overflow(int c) {
-    if (c != EOF) {
-        _buffer += static_cast<char>(c);
-    } else {
-        sync();
-    }
-    return c;
-}
-
-File::File(const Priority &priority, std::shared_ptr<std::pair<std::ofstream, std::mutex>> out) : _priority(priority){
-    _out = out;
-    _header = "";
-}
-
-File::File(const Priority &priority, const std::string &header, std::shared_ptr<std::pair<std::ofstream, std::mutex>> out) : _priority(priority){
-    _out = out;
-    _header = header;
-}
-
-int File::sync() {
-    if (!_buffer.empty() && (_priority <= GLOBAL_PRIORITY)) {
-        std::unique_lock<std::mutex> l(_out->second);
-        _out->first << "["
-            << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count()
-            << "] "
-            << _buffer;
-        _out->first.flush();
-    }
-    _buffer.erase();
-    return 0;
-}
-
-int File::overflow(int c) {
-    if (c != EOF) {
-        _buffer += static_cast<char>(c);
-    } else {
-        sync();
-    }
-    return c;
-}
+Priority GLOBAL_PRIORITY = kLogCrit;
 
 }
--- src/slog.h
+++ src/slog.h
@@ -7,6 +7,8 @@
 #include <mutex>
 #include <syslog.h>
 
+#include <iostream>
+
 //The following is mostly stolen from http://stackoverflow.com/a/4457138
 
 namespace slog{
@@ -24,49 +26,60 @@ enum Priority {
     kLogDebug   = LOG_DEBUG    // debug-level message
 };
 
-Priority GLOBAL_PRIORITY;
+extern Priority GLOBAL_PRIORITY;
 
-class Syslog : public Char_Stream {
-    private:
-        std::string _buffer;
-        Priority _priority;
-
-    protected:
-        int sync();
-        int overflow(int c);
-        std::string _header;
+class Log_Sink{
 
     public:
-        explicit Syslog(const Priority& priority, const std::string &header, const std::string& ident, const int& facility);
-        explicit Syslog(const Priority& priority, const std::string& ident, const int& facility);
+        virtual void write(const std::string &msg) = 0;
+
 };
 
-class File : public Char_Stream {
+class Log_Buffer : public Char_Stream {
+
     private:
         std::string _buffer;
-        std::shared_ptr<std::pair<std::ofstream, std::mutex>> _out;
-        Priority _priority;
+        std::shared_ptr<Log_Sink> _sink;
+        const Priority _priority;
+        const std::string _header;
 
     protected:
-        int sync();
-        int overflow(int c);
-        std::string _header;
+
+        int sync(){
+            if (!_buffer.empty() && (_priority <= GLOBAL_PRIORITY)) {
+                _sink->write(_buffer);
+            }
+            _buffer = _header;
+            return 0;
+        }
+        int overflow(int c){
+            if (c != EOF) {
+                _buffer += static_cast<char>(c);
+            } else {
+                sync();
+            }
+            return c;
+        };
 
     public:
-        explicit File( const Priority &priority, const std::string &header, std::shared_ptr<std::pair<std::ofstream, std::mutex>> out);
-        explicit File( const Priority &priority, std::shared_ptr<std::pair<std::ofstream, std::mutex>> out);
-};
-
-class Log : public std::ostringstream {
-    private:
-        std::shared_ptr<Char_Stream> _backend;
-
-    public:
-        explicit Log(std::shared_ptr<Char_Stream> backend){
-            _backend = backend;
-            (static_cast<std::ostream*>(this))->rdbuf(_backend.get());
+        Log_Buffer(std::shared_ptr<Log_Sink> sink, const Priority &priority, const std::string &header):
+            _priority(priority),
+            _header(header)
+        {
+            _sink = sink;
+            _buffer = _header;
         }
 
+};
+
+class Log : public std::basic_ostringstream<char> {
+    private:
+        Log_Buffer _backend;
+
+    public:
+        explicit Log(std::shared_ptr<Log_Sink> sink, const Priority &priority, const std::string &header) : _backend(sink, priority, header){
+            (static_cast<std::ostream*>(this))->rdbuf(&_backend);
+        }
 };
 
 }
