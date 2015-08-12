2a9f120b8296a3a20fa2fc14d87a7a8582af1aff
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-08-11 20:43:27 -0400

    Make timestamps on File toggleable and off by default

--- src/file.cc
+++ src/file.cc
@@ -9,16 +9,26 @@ namespace slog{
 
     void File::write(const std::string &msg){
         std::unique_lock<std::mutex> l(_backend->second);
-        char time[255];
-        const auto current_time = std::chrono::high_resolution_clock::now();
-        const std::time_t t = std::chrono::high_resolution_clock::to_time_t(current_time);
-        //The following is not threadsafe due to std::localtime's use of some global storage
-        //I don't care to fix this at the moment
-        std::strftime(time, 255, "%b %d %H:%M:%S", std::localtime(&t));
+        if(_timestamp){
+            char time[255];
+            const auto current_time = std::chrono::high_resolution_clock::now();
+            const std::time_t t = std::chrono::high_resolution_clock::to_time_t(current_time);
+            //The following is not threadsafe due to std::localtime's use of some global storage
+            //I don't care to fix this at the moment
+            std::strftime(time, 255, "%b %d %H:%M:%S", std::localtime(&t));
 
-        _backend->first << time;
+            _backend->first << time;
+        }
         _backend->first << msg;
         _backend->first.flush();
+    }
+
+    void File::enable_timestamp(){
+        _timestamp = true;
+    }
+
+    void File::disable_timestamp(){
+        _timestamp = false;
     }
 
 }
--- src/file.h
+++ src/file.h
@@ -11,11 +11,14 @@ namespace slog{
 class File : public Log_Sink{
 
     private:
+        bool _timestamp = false;
         std::shared_ptr<std::pair<std::ofstream, std::mutex>> _backend;
 
     public:
         File(std::shared_ptr<std::pair<std::ofstream, std::mutex>> backend);
         void write(const std::string &msg);
+        void enable_timestamp();
+        void disable_timestamp();
 
 };
 
