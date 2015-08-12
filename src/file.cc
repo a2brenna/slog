c0ed092d5892f88532908bbb51bce0a23c5cae54
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-08-11 20:29:39 -0400

    Add timestamps to File backend

--- src/file.cc
+++ src/file.cc
@@ -1,4 +1,5 @@
 #include "file.h"
+#include <chrono>
 
 namespace slog{
 
@@ -8,6 +9,14 @@ namespace slog{
 
     void File::write(const std::string &msg){
         std::unique_lock<std::mutex> l(_backend->second);
+        char time[255];
+        const auto current_time = std::chrono::high_resolution_clock::now();
+        const std::time_t t = std::chrono::high_resolution_clock::to_time_t(current_time);
+        //The following is not threadsafe due to std::localtime's use of some global storage
+        //I don't care to fix this at the moment
+        std::strftime(time, 255, "%b %d %H:%M:%S", std::localtime(&t));
+
+        _backend->first << time;
         _backend->first << msg;
         _backend->first.flush();
     }
